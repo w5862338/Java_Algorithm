9bbdacbe4771148741bef28c316d71548518f858
wx <w5862338@126.com>
2019-01-13 18:07:26 +0800

    开始项目

---
+++ begin/src/begin/hello.java
@@ -0,0 +1,12 @@
+package begin;
+/*
+ *@Description
+ *@Author  wx
+ * @Date   2019-01-13-18:05
+ */
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Algorithm ");
+    }
+}
